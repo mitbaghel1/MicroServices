3aa1870c3a71fe044f5bc75c89ed4b0fa73e185d
mitbaghel1 <mitbaghel6@gmail.comYexitgit config --global user.email mitbaghel6@gmail.com>
2022-07-24 12:06:45 +0530

    check Commit

--- MS/src/main/java/com/ms/MSApplication.java
+++ MS/src/main/java/com/ms/MSApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MSApplication {
  public static void main(String[] args) {
+	 System.out.println("HI");
 	 SpringApplication.run(MSApplication.class, args);
+	 
  }
 }
