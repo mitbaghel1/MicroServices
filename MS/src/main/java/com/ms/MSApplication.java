8e2ac3442a7ce62b269244fa7c3198055ab0f84a
MIT BAGHEL <59555598+mitbaghel1@users.noreply.github.com>
2022-07-31 19:30:33 +0530

    demo commit

--- MS/src/main/java/com/ms/MSApplication.java
+++ MS/src/main/java/com/ms/MSApplication.java
@@ -2,13 +2,8 @@ package com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@EntityScan(basePackages = {"com.ms.entity"})
-@EnableJpaRepositories(basePackages = {"com.ms.repository"})
-@SpringBootApplication(scanBasePackages= {"com.ms.*"})
+@SpringBootApplication
 public class MSApplication {
  public static void main(String[] args) {
 	 SpringApplication.run(MSApplication.class, args);
--- MS/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-insert into user value(1,'mit',SYSDATE());
